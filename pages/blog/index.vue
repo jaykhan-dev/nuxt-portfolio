6c5082cad35689e12f7f1f69d7b308c3b7239639
khanr <khanriza@gmail.com>
2020-08-28 13:22:08 -0400

    Test Original Code Again

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -24,9 +24,11 @@
           </v-card-text>
           <v-card-actions>
             <v-spacer></v-spacer>
-            <v-btn :to="blog.path" color="orange">
-              Read More
-            </v-btn>
+            <NuxtLink :to="blog.path">
+              <v-btn color="primary">
+                Read More
+              </v-btn>
+            </NuxtLink>
           </v-card-actions>
         </v-card>
       </v-col>
