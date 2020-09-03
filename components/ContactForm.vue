bad2351e4290cf1084ac610aa125f2db503d70ec
khanr <khanriza@gmail.com>
2020-09-03 09:11:15 -0400

    Form Adjustment

--- components/ContactForm.vue
+++ components/ContactForm.vue
@@ -1,48 +1,48 @@
 <template>
   <div>
-    <form name="contact" method="POST" data-netlify="true">
-      <p>
-        <label>Your Name: <input type="text" name="name" /></label>
-      </p>
-      <p>
-        <label>Your Email: <input type="email" name="email" /></label>
-      </p>
-      <p>
-        <label>Message: <textarea name="message"></textarea></label>
-      </p>
-      <p>
-        <button type="submit">Send</button>
-      </p>
-    </form>
-    <!-- <form action="/success" name="contact" method="POST" data-netlify="true"> -->
-    <!--   <v-container> -->
-    <!--     <v-row> -->
-    <!--       <v-col cols="12" md="6"> -->
-    <!--         <v-text-field -->
-    <!--           name="fullname" -->
-    <!--           label="Full Name" -->
-    <!--           class="pa-0 ma-0" -->
-    <!--           type="text" -->
-    <!--           required -->
-    <!--           outlined -->
-    <!--         ></v-text-field> -->
-    <!--         <v-text-field -->
-    <!--           name="email" -->
-    <!--           label="E-mail" -->
-    <!--           type="email" -->
-    <!--           required -->
-    <!--           outlined -->
-    <!--         ></v-text-field> -->
-    <!--       </v-col> -->
-    <!--       <v-col cols="12" md="6"> -->
-    <!--         <v-textarea outlined name="message" label="Message"></v-textarea> -->
-    <!--       </v-col> -->
-    <!--     </v-row> -->
-    <!--     <v-row> -->
-    <!--       <v-btn type="submit" class="ml-auto primary">Submit</v-btn> -->
-    <!--     </v-row> -->
-    <!--   </v-container> -->
+    <!-- <form name="contact" method="POST" data-netlify="true"> -->
+    <!--   <p> -->
+    <!--     <label>Your Name: <input type="text" name="name" /></label> -->
+    <!--   </p> -->
+    <!--   <p> -->
+    <!--     <label>Your Email: <input type="email" name="email" /></label> -->
+    <!--   </p> -->
+    <!--   <p> -->
+    <!--     <label>Message: <textarea name="message"></textarea></label> -->
+    <!--   </p> -->
+    <!--   <p> -->
+    <!--     <button type="submit">Send</button> -->
+    <!--   </p> -->
     <!-- </form> -->
+    <form name="contact" method="POST" data-netlify="true" action="/success">
+      <v-container>
+        <v-row>
+          <v-col cols="12" md="6">
+            <v-text-field
+              name="fullname"
+              label="Full Name"
+              class="pa-0 ma-0"
+              type="text"
+              required
+              outlined
+            ></v-text-field>
+            <v-text-field
+              name="email"
+              label="E-mail"
+              type="email"
+              required
+              outlined
+            ></v-text-field>
+          </v-col>
+          <v-col cols="12" md="6">
+            <v-textarea outlined name="message" label="Message"></v-textarea>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-btn type="submit" class="ml-auto primary">Submit</v-btn>
+        </v-row>
+      </v-container>
+    </form>
   </div>
 </template>
 
