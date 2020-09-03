a57be91f9a11d2c0fbabf0040a5ca8a0e33ecb40
khanr <khanriza@gmail.com>
2020-09-03 09:35:00 -0400

    Additional Form Styling Adjustments

--- components/ContactForm.vue
+++ components/ContactForm.vue
@@ -1,11 +1,27 @@
 <template>
   <div>
-    <form name="contact" method="POST" data-netlify="true">
-      <v-text-field type="text" name="name"></v-text-field>
-      <v-text-field type="email" name="email"></v-text-field>
-      <v-textarea name="message"></v-textarea>
-      <v-btn type="submit">Send</v-btn>
-    </form>
+    <v-container>
+      <v-row>
+        <v-col cols="12" lg="6" offset-lg="4">
+          <form name="contact" method="POST" data-netlify="true">
+            <v-text-field
+              type="text"
+              name="name"
+              label="Full Name"
+              oulined
+            ></v-text-field>
+            <v-text-field
+              type="email"
+              name="email"
+              label="Email"
+              oulined
+            ></v-text-field>
+            <v-textarea name="message" label="Message" outlined></v-textarea>
+            <v-btn type="submit">Send</v-btn>
+          </form>
+        </v-col>
+      </v-row>
+    </v-container>
     <!-- <form name="contact" method="POST" data-netlify="true" action="/success"> -->
     <!--   <v-container> -->
     <!--     <v-row> -->
