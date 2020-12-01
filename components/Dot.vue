4f4870c87579f0c7e4fdf5189592df1fd57312e0
khanr <khanriza@gmail.com>
2020-11-30 21:23:01 -0500

    added new Dot component

---
+++ components/Dot.vue
@@ -0,0 +1,65 @@
+<template>
+  <svg width="30" :style="{ top: x, left: y, opacity: o }" height="30">
+    <circle cx="15" cy="15" :r="r" stroke-width="4" fill="white" />
+  </svg>
+</template>
+
+<script>
+export default {
+  props: {
+    x: {
+      type: Number,
+      required: true,
+    },
+    y: {
+      type: Number,
+      required: true,
+    },
+    r: {
+      type: Number,
+      required: true,
+    },
+    o: {
+      type: Number,
+      required: true,
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+svg {
+  position: absolute;
+}
+
+.twinkle {
+  transition: all 0.5s ease;
+  animation: twinkle 2s ease forwards;
+}
+
+@keyframes fade-in {
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+}
+
+@keyframes twinkle {
+  0% {
+    transform: scale(1);
+    background: grey;
+  }
+
+  50% {
+    transform: scale(1.5);
+    background: white;
+  }
+
+  100% {
+    transform: scale(1);
+    background: grey;
+  }
+}
+</style>
