912f212451845cac075328cb856ec9f0f5533050
khanr <khanriza@gmail.com>
2020-12-07 19:29:07 -0500

    added categores

---
+++ components/Categories.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-row>
+    <v-col cols="12">
+      <h2 class="ma-0">Categories</h2>
+    </v-col>
+    <v-col cols="12">
+      <v-btn
+        v-for="(category, index) in categories"
+        :key="index"
+        class="ma-1"
+        :class="selectedCategory === category ? 'primary' : ''"
+        @click="selectCategory(category)"
+      >
+        {{ category }}</v-btn
+      >
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  props: {
+    categories: {
+      type: Array,
+      required: true,
+    },
+    selectedCategory: {
+      type: String,
+      required: false,
+      default: '',
+    },
+  },
+  methods: {
+    selectCategory(category) {
+      if (category === this.selectedCategory) {
+        this.$emit('selectCategory', '')
+      } else {
+        this.$emit('selectCategory', category)
+      }
+    },
+  },
+}
+</script>
+
+<style></style>
