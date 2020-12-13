e131dd683f2d8a5fef121e36863e9ae644766e43
khanr <khanriza@gmail.com>
2020-12-13 11:20:01 -0500

    remove prev next items and add store commits

--- pages/blog/_slug.vue
+++ pages/blog/_slug.vue
@@ -1,34 +1,7 @@
 <template>
   <v-container>
     <v-row>
-      <v-col cols="12" lg="3">
-        <TableOfContents :toc="doc.toc" sticky="true" />
-
-        <h3>Related Articles:</h3>
-        <NuxtLink
-          v-if="prev"
-          :to="{ name: 'blog-slug', params: { slug: prev.slug } }"
-          class="white-text"
-        >
-          <p class="white--text">
-            {{ prev.title }}
-          </p>
-        </NuxtLink>
-        <NuxtLink
-          v-if="next"
-          :to="{ name: 'blog-slug', params: { slug: next.slug } }"
-        >
-          <p class="white--text">
-            {{ next.title }}
-          </p>
-        </NuxtLink>
-      </v-col>
-      <v-col cols="12" lg="9">
-        <v-row class="d-flex justify-space-between">
-          <v-col> </v-col>
-
-          <v-col class="text-right"> </v-col>
-        </v-row>
+      <v-col cols="12">
         <nuxt-content :document="doc" />
       </v-col>
     </v-row>
@@ -36,12 +9,9 @@
 </template>
 
 <script>
-import TableOfContents from '@/components/TableOfContents'
 export default {
-  components: {
-    TableOfContents,
-  },
-  async asyncData({ $content, params, redirect }) {
+  layout: 'article',
+  async asyncData({ $content, params, redirect, store }) {
     const doc = await $content('blog', params.slug).fetch()
 
     const [prev, next] = await $content('blog')
@@ -54,7 +24,11 @@ export default {
     if (!doc.published) {
       redirect('/blog')
     }
-    return { doc, prev, next }
+
+    store.commit('blog/SET_BLOG', doc)
+    store.commit('blog/SET_NEXT', next)
+    store.commit('blog/SET_PREV', prev)
+    return { doc }
   },
 }
 </script>
