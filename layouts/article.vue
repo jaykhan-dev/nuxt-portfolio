4bfa318b2638b893181ab5d4a6725418834243b4
khanr <khanriza@gmail.com>
2020-12-13 11:20:24 -0500

    layout adjustments

---
+++ layouts/article.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-app id="inspire">
+    <v-navigation-drawer v-model="drawer" app class="py-1 grey darken-4">
+      <TableOfContents :toc="doc.toc" :prev="prev" :next="next" sticky="true" />
+    </v-navigation-drawer>
+    <v-app-bar app class="hidden-lg-and-up">
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+    </v-app-bar>
+    <v-main>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import TableOfContents from '@/components/TableOfContents'
+import { mapState } from 'vuex'
+export default {
+  components: {
+    TableOfContents,
+  },
+  data: () => ({ drawer: null }),
+  computed: {
+    ...mapState({
+      doc: (state) => state.blog.blog,
+      prev: (state) => state.blog.prev,
+      next: (state) => state.blog.next,
+    }),
+  },
+}
+</script>
+
+<style lang="scss"></style>
