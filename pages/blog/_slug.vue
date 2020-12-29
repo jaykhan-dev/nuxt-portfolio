de28e95ccf5e96fb34c8a0340ee3d23bbc1a4413
khanr <khanriza@gmail.com>
2020-12-28 22:33:10 -0500

    adjusted column for larger views

--- pages/blog/_slug.vue
+++ pages/blog/_slug.vue
@@ -1,7 +1,7 @@
 <template>
   <v-container>
-    <v-row>
-      <v-col cols="12">
+    <v-row class="justify-center">
+      <v-col cols="12" md="10" lg="6">
         <nuxt-content :document="doc" />
       </v-col>
     </v-row>
